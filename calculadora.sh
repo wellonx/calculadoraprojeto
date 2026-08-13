52f82a5a91c69150bbd6dcf04eb12693e66d8efd
Willon Macedo <willoncmacedo@gmail.com>
2026-08-13 00:30:05 -0300

    Calculadora

---
+++ calculadora.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Executando script..."
+
+
+echo "Instalando python"
+
+sudo apt update
+sudo apt install python3
+
+
+
+echo "Abrindo a calculadora"
+
+python3 calculadora.py
+
